2aa7b0a25917bb8c2522ddcaaa3afb90bd2c81df
WJDavies1 <will10davies@hotmail.co.uk>
2024-08-12 11:36:54 +0100

    first commit

---
+++ datanotes.py
@@ -0,0 +1,61 @@
+print("hello world")
+
+
+print("this is my code") 
+
+print("thanks for listening") 
+
+
+print("hello again")
+
+# print ("this is a string?![]")
+
+# print ("1234 - still a string because got speach marks")
+
+print( 1234+10) 
+
+
+print("hello world".upper())
+
+
+
+
+
+print(len("hello world")) 
+
+print("hello world"[1])
+
+print("   |   |   ")
+print("   |   |   ")
+print("   |   |   ")
+print("-----------")
+print("   |   |   ")
+print("   |   |   ")
+print("   |   |   ")
+print("-----------")
+print("   |   |   ")
+print("   |   |   ")
+print("   |   |   ")
+
+
+print("HELLO WORLD".lower())
+#puts all characters in string in lowercase
+print("hello, and welcome world".capitalize())
+#converts the first character to upper case
+
+print("the world is great, i love the world".count("world"))
+#returns the number of times a specified value apears in the string 
+
+print("hello world, welcome to VSC".find("world"))
+# finds first occurance of specified value 
+
+print("i like the earth".replace("earth","world"))
+#method replaces specified phrase with another specified phrase
+
+
+print("       world     ".strip())
+#method remoes and leading, and trailing whitespaces. leading means at beginning and trailing means atend 
+
+print("All around the world"[7].upper())
+
+
---
+++ variables.py
@@ -0,0 +1,5 @@
+my_name = "Will"
+
+print(my_name) 
+
+p
